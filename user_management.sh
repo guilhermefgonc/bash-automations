c946cab10832a9775765e3cb1f7d0474d5c1595e
guilhermefgonc <125393251+guilhermefgonc@users.noreply.github.com>
2024-04-03 16:41:47 -0300

    Create user_management.sh

---
+++ user_management.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# the IP of the servers are in inventory.txt
+servers=$(cat inventory.txt)
+
+# read the username and userid
+echo -n "Enter the user name:"
+read username
+echo -n "Enter the user id:"
+read userid
+
+# loop through all the servers, and add the new user to each server
+for i in $servers; do
+	echo $i # display the name of the current server of the list
+	ssh  $i "sudo useradd -m -u $userid $username" # access the server and add the user
+	# error checking
+	if [ $? -eq 0 ]; then
+		echo "User $username is add sucessfully on $i"
+	else
+		echo "Error on $i"
+	fi
+done
